607dde005a39bd80e5becb0b5c420c6f05a4eae5
lavanya <lavvik25@gmail.com>
2026-04-05 12:41:23 -0400

    env variables added

--- tests/web-based-authentication.spec.js
+++ tests/web-based-authentication.spec.js
@@ -12,13 +12,15 @@ test("Bypass authentication by embedding the credentials in the URL", async ({
 test("Bypass authentication by encoding the credentials base64 format", async ({
   page,
 }) => {
-  let encodedCredential = Buffer.from("admin:admin").toString("base64");
+  let encodedCredential = Buffer.from(
+    `${process.env.PRACTICE_USERNAME}:${process.env.PRACTICE_USERNAME}`,
+  ).toString("base64");
 
   await page.setExtraHTTPHeaders({
     Authorization: `Basic ${encodedCredential}`,
   });
 
-  page.goto("https://the-internet-5chk.onrender.com/basic_auth");
+  page.goto("https://the-internet-5chk.onrender.com/ basic_auth");
 
   await page.waitForTimeout(3000);
 });
